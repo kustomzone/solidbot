1530f32f05dbaf672d40eca90753a7dfbee97ab9
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-15 15:10:50 +0200

    Add worker and delays

--- bin/sodidbot.js
+++ bin/sodidbot.js
@@ -1,30 +1,52 @@
 #!/usr/bin/env node
 
-var CronJob = require('cron').CronJob
+var CronJob       = require('cron').CronJob
+var child_process = require('child_process')
+var kue           = require('kue')
 
-var kue = require('kue')
-
-var queue = kue.createQueue()
-
+// init
 var interval = 4
+var queue    = kue.createQueue()
 
+
+// cron
 new CronJob('*/'+ interval +' * * * * *', function() {
   console.log('Running Cron every '+ interval +' seconds')
 
-  queue.inactiveCount( function( err, total ) { // others are activeCount, completeCount, failedCount, delayedCount
-  console.log(total);
-});
 
 }, null, true, 'America/Los_Angeles')
 
+
+queue.process('cmd', function(job, done){
+  console.log(job.data)
+  console.log('running : ' + job.data.title);
+  var cmd = job.data.cmd
+  child_process.exec(cmd, function(err, stdout, stderr){
+    if (err) {
+      console.error(err);
+      setTimeout(done, interval*1000)
+      //done()
+    } else {
+      console.log(stdout);
+      setTimeout(done, interval*1000)
+      //done()
+    }
+  })
+})
+
+
+queue.activeCount( function( err, total ) { // others are activeCount, completeCount, failedCount, delayedCount
+  console.log(total)
+  if (total === 0) {
+    setTimeout(nextJob, interval*1000)
+  }
+})
+
+
+// server
 kue.app.listen(3002)
 
-/*
-var job = queue.create('test', {
-    title: 'test job'
-  , description: 'just a test'
-  , extra: 'extra field'
-}).save( function(err){
-   if( !err ) console.log( job.id );
-})
-*/
+
+// process
+function nextJob() {
+}
