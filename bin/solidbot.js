b85c7c5c53f8fedad754c92d02cd9bbcd82f3d5a
Melvin Carvalho <melvincarvalho@gmail.com>
2016-09-13 00:16:57 +0200

    Add configurable queues closes #16

--- bin/solidbot.js
+++ bin/solidbot.js
@@ -23,9 +23,11 @@ new CronJob('*/' + interval + ' * * * * *', function () {
 var program = commander
   .option('-p --port [num]', 'Port Number')
   .option('-c --crawl <sites>', 'Comma separted set of sites job with crawl/site')
+  .option('-q --queues <queues>', 'Comma separated set of queues to use, overrides default')
   .parse(process.argv)
 
 var crawlSites = program.crawl
+var queues = program.queues
 
 if (crawlSites) {
   var sites = crawlSites.split(',')
@@ -35,11 +37,31 @@ if (crawlSites) {
   }
 }
 
-queue.process('db', db)
-queue.process('chain', chain)
-queue.process('cmd', cmd)
-queue.process('crawl', crawl)
-queue.process('inbox', inbox)
+if (queues) {
+  var q = queues.split(',')
+
+  if (q.indexOf('db') !== -1) {
+    queue.process('db', db)
+  }
+  if (q.indexOf('chain') !== -1) {
+    queue.process('chain', chain)
+  }
+  if (q.indexOf('cmd') !== -1) {
+    queue.process('cmd', cmd)
+  }
+  if (q.indexOf('crawl') !== -1) {
+    queue.process('crawl', crawl)
+  }
+  if (q.indexOf('inbox') !== -1) {
+    queue.process('inbox', inbox)
+  }
+} else {
+  queue.process('db', db)
+  queue.process('chain', chain)
+  queue.process('cmd', cmd)
+  queue.process('crawl', crawl)
+  queue.process('inbox', inbox)
+}
 
 var defaultPort = 3005
 var port = program.port || defaultPort
